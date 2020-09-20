b1a9348511977099c6476731c0c2714e702f40a8
bhanukarandeniya <bhanukarandeniya@gmail.com>
2020-09-21 00:11:26 +0530

    Application server test method added

--- src/route/index.js
+++ src/route/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const covidIncidentRouter = require('./covid-incident-route');
 const personRouter = require('./person-route');
 const districtRouter = require('./distric-route');
+const { testApp } = require('../controller/test-controller');
 const { swaggerUi, swaggerCovidDoc, swaggerPersonDoc } = require('../config/swagger-config');
 
 
@@ -14,5 +15,8 @@ router.use("/api-docs/person", swaggerUi.serve, swaggerUi.setup(swaggerPersonDoc
 router.use('/covid-incidents', covidIncidentRouter);
 router.use('/person', personRouter);
 router.use('/districts', districtRouter);
+
+//Application Server Test
+router.get('/test', testApp);
 
 module.exports = router;
